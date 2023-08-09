ef453cb7a6d343bb719de7c23805e937b7744052
jsong98 <jswsn1526@gmail.com>
2023-08-09 17:38:07 +0900

    junseok folder edit

---
+++ topic/stack-queue/junseok/MinimumRemoveToMake.java
@@ -0,0 +1,58 @@
+package topic.stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MinimumRemoveToMake {
+
+	public static void main(String[] args) {
+		String s = "))((";
+		Queue<Character> que = new LinkedList<Character>();
+		Stack<Character> stCnt = new Stack<>(); 	// '('와 ')'를 카운팅하기 위한 스택
+		
+		char cnt = '0';					// 스택에 넣을 카운팅 변수
+		char[] sArr = s.toCharArray();	// 함수의 파라미터를 char 배열로 변환
+		int len = 0;					// 리턴할 String의 길이
+		int lCnt = 0;					// 리턴할 String을 만들 때 필요한 '('의 개수를 저장하는 변수
+		int rCnt = 0;					
+		String result = "";				// 리턴할 String 선언
+		
+		for(int i = 0; i < sArr.length; i++) {
+			if(sArr[i] != '(' && sArr[i] != ')') {	// parentheses가 아닌 경우
+				que.offer(sArr[i]);
+				len++;
+			}
+			
+			if(sArr[i] == '(') {	// '('인 경우
+				que.offer(sArr[i]);
+				stCnt.push(cnt);
+				len++;
+			}
+			
+			if(sArr[i] == ')' && !stCnt.isEmpty()) {	// ')'인 경우
+				que.offer(sArr[i]);
+				stCnt.pop();
+				len++;
+				rCnt++;
+			}
+		}
+		
+		for(int j = 0; j < len ; j++) {		// 리턴할 String 조립
+			String k = Character.toString(que.poll());
+			
+			if(k.equals("(") && lCnt < rCnt) {
+				result += k;
+				lCnt++;
+			} else if(k.equals("(") && lCnt >= rCnt) {
+				result += "";
+			} else {
+				result += k;
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
---
+++ topic/stack-queue/junseok/ValidParentheses.java
@@ -0,0 +1,81 @@
+package topic.stack_queue;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+		String s = "[([]])";
+		char[] arr = s.toCharArray();		// 입력을 char Array로 변환
+		boolean isValid = true;				// 리턴 값
+		int open = 0;						// 열려 있는 괄호를 나타내는 변수
+		
+		Stack<Character> s1 = new Stack<>();	// 각 괄호가 열려있는지 여부를 check하는 stack
+		Stack<Character> s2 = new Stack<>();
+		Stack<Character> s3 = new Stack<>();
+		Stack<Integer> openSt = new Stack<>();	// 모든 괄호를 포함, 열려 있는 괄호를 check하는 stack 
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			if(arr[i] == '(') {	
+				s1.push(arr[i]);		// 해당 stack에 추가 --> 이 괄호가 아직 열려있음을 나타냄
+				open = 1;				// open에 해당 괄호를 나타내는 값을 대입
+				openSt.push(open);		// open을 stack에 저장 --> 해당 괄호가 마지막으로 열림을 나타냄
+			} else if(arr[i] == ')' && !s1.isEmpty() && (open == 1 || open == 0)) {
+				// 닫힘 괄호가 나왔을 때, 이 괄호의 스택이 비어있지 않고, 이 괄호가 마지막으로 열렸거나 앞선 괄호가 닫힌 상태일 때
+				s1.pop();				// 해당 stack에서 pop 한번 해주고
+				openSt.pop();			// open 스택에서도 pop 해줌
+				if(!openSt.isEmpty()) {	// open 스택이 비어있지 않으면
+					open = openSt.peek();	// open 스택의 맨 위 값 = 마지막으로 열린 괄호를 open에 대입
+				} else {					// 아무 괄호도 열려있지 않으면 0을 대입
+					open = 0;
+				}
+			} else if(arr[i] == ')' && (s1.isEmpty() || !(open == 1))) {
+				isValid = false;
+				break;
+			}
+			
+			if(arr[i] == '{') {
+				s2.push(arr[i]);
+				open = 2;
+				openSt.push(open);
+			} else if(arr[i] == '}' && !s2.isEmpty() && (open == 2 || open == 0)) {
+				s2.pop();
+				openSt.pop();
+				if(!openSt.isEmpty()) {
+					open = openSt.peek();
+				} else {
+					open = 0;
+				}
+			} else if(arr[i] == '}' && (s2.isEmpty() || !(open == 2))) {
+				isValid = false;
+				break;
+			}
+			
+			if(arr[i] == '[') {
+				s3.push(arr[i]);
+				open = 3;
+				openSt.push(open);
+			} else if(arr[i] == ']' && !s3.isEmpty() && (open == 3 || open == 0)) {
+				s3.pop();
+				openSt.pop();
+				if(!openSt.isEmpty()) {
+					open = openSt.peek();
+				} else {
+					open = 0;
+				}
+			} else if(arr[i] == ']' && (s3.isEmpty() || !(open == 3))) {
+				isValid = false;
+				break;
+			}
+			
+			if(i == arr.length-1 && (!s1.isEmpty() || !s2.isEmpty() || !s3.isEmpty())) {
+				isValid = false;
+			}
+		}
+		
+		System.out.println(isValid);
+		
+	}
+
+}
